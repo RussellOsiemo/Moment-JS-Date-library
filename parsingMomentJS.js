7d8d8b69c68a55f39e4978b61aa3ff78a282fc3d
Russel Osiemo <86182360+RussellOsiemo@users.noreply.github.com>
2021-11-05 11:17:48 +0300

    moment.mi.js file

--- parsingMomentJS.js
+++ parsingMomentJS.js
@@ -1,2 +1,7 @@
 //parsing is the acting of converting information to an object
+
+const moment = require("./moment.min");
+
 //creating moment for current date and time
+let t = moment();
+console.log(moment().format());
